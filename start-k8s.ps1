909800e6632da234a9025c53490ac676012cd44d
NDI6FE <cy.rae@yahoo.com>
2024-09-13 10:01:51 +0200

    Added powershell script

--- start-k8s.ps1
+++ start-k8s.ps1
@@ -1,30 +1,29 @@
-#!/bin/bash
-
 # Start minikube if it is not already running
-if ! minikube status &>/dev/null; then
-  echo "Start minikube..."
-  minikube start
-else
-  echo "Minikube is already running. Skip start..."
-fi
+$minikubeStatus = minikube status
+if ($minikubeStatus -like "*Running*") {
+    Write-Output "Minikube is already running. Skip start..."
+} else {
+    Write-Output "Start minikube..."
+    minikube start
+}
 
 # Delete existing namespace if it exists
-if kubectl get namespace dhbw-tube &>/dev/null; then
-  echo "Deleting existing dhbw-tube namespace..."
-  kubectl delete namespace dhbw-tube
-  # Wait for the namespace to be completely deleted
-  while kubectl get namespace dhbw-tube &>/dev/null; do
-    echo "Waiting for dhbw-tube namespace to be deleted..."
-    sleep 2
-  done
-fi
+if (kubectl get namespace dhbw-tube -ErrorAction SilentlyContinue) {
+    Write-Output "Deleting existing dhbw-tube namespace..."
+    kubectl delete namespace dhbw-tube
+    # Wait for the namespace to be completely deleted
+    while (kubectl get namespace dhbw-tube -ErrorAction SilentlyContinue) {
+        Write-Output "Waiting for dhbw-tube namespace to be deleted..."
+        Start-Sleep -Seconds 2
+    }
+}
 
 # Enable the ingress addon in Minikube
-echo "Enabling Ingress addon in Minikube..."
+Write-Output "Enabling Ingress addon in Minikube..."
 minikube addons enable ingress
 
 # Create docker images
-eval $(minikube docker-env)
+& minikube -p minikube docker-env | Invoke-Expression
 docker build -t dhbw-tube-frontend ./dhbw-tube-frontend
 docker build -t dhbw-tube-stream ./dhbw-tube-stream
 docker build -t dhbw-tube-upload ./dhbw-tube-upload
@@ -62,5 +61,5 @@ kubectl apply -f k8s/frontend/frontend-service.yaml
 kubectl apply -f k8s/ingress.yaml
 
 # Retrieve the minikube IP
-echo "Minikube IP:"
+Write-Output "Minikube IP:"
 minikube ip
