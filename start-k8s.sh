e7b89eb0d5d5857bbb9dd7b724d89b693c3a0631
Dario Nieddu <cy.rae@yahoo.com>
2024-09-17 17:37:25 +0200

    Bug fix

--- start-k8s.sh
+++ start-k8s.sh
@@ -19,7 +19,30 @@ if kubectl get namespace dhbw-tube &>/dev/null; then
   done
 fi
 
+# Check if prometheus is added to the helm repositories. If not add it.
+if helm repo list | grep -q "prometheus-community"; then
+  echo "Prometheus helm repository is already added..."
+else
+  echo "Adding Prometheus helm repository..."
+  helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
+  helm repo update
+fi
+
+# Check if grafana is added to the helm repositories. If not add it.
+if helm repo list | grep -q "grafana"; then
+  echo "Grafana helm repository is already added..."
+else
+  echo "Adding Grafana helm repository..."
+  helm repo add grafana https://grafana.github.io/helm-charts
+  helm repo update
+fi
+
+# Create kubernetes environment
 skaffold dev
+
+# Install prometheus and grafana in dhbw-tube namespace with specific ports
+helm install prometheus prometheus-community/prometheus --namespace dhbw-tube --set server.service.port=8888
+helm install grafana grafana/grafana --namespace dhbw-tube --set service.nodePort=9999 --set adminPassword=admin
 
 # Print the URL to access the application
 echo "DHBW-Tube runs on: http://localhost/"
